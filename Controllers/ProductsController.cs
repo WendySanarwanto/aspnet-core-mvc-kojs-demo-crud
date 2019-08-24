edc21d92eaf7f04e3ab172ca461ff5117f6314cf
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-08-25 03:29:08 +0800

    Convert the Products  controller's index action to use async awai

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 using aspnet_core_mvc_kojs_demo_crud.Repositories;
 using aspnet_core_mvc_kojs_demo_crud.ViewModels;
@@ -6,12 +7,15 @@ using aspnet_core_mvc_kojs_demo_crud.ViewModels;
 namespace aspnet_core_mvc_kojs_demo_crud.Controllers 
 {
     public class ProductsController : Controller {
-        public IActionResult Index() {
-            var productsRepo = new ProductRepository();
-            var productsListViewModel = new ProductsListViewModel(
-                productsRepo.FindAll()
-            );
-            return View(productsListViewModel);
+        public async Task<IActionResult> Index() {
+            return await Task.Run(() => {
+                var productsRepo = new ProductRepository();
+                var productsListViewModel = new ProductsListViewModel(
+                    productsRepo.FindAll()
+                );                
+                return View(productsListViewModel);
+            });
         }
+
     }
 }
