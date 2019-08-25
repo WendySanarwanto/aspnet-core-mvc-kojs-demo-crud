20be9e9b205f5b83f9e30ec1b2dba000b9870647
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-08-25 11:17:31 +0800

    Added logic to filter Products from other Products that are marked as IsDeleted

--- wwwroot/js/ko/products.view-model.js
+++ wwwroot/js/ko/products.view-model.js
@@ -5,15 +5,23 @@ class ProductsViewModel {
             var _number = 1;
             var products = model.Products.map(p => {
                 const numberedProduct = {                    
-                    Number: _number,
-                    ...p
+                    Number: ko.observable(_number),
+                    ...p,
+                    IsDirty: ko.observable(false),
+                    IsDeleted: ko.observable(false),
                 };
 
                 _number++;
                 return numberedProduct;
             });
-            self.products = ko.observableArray(products);
+            self.products = ko.mapping.fromJS(products);
             console.log(`[debug] - self.products(): \n`, self.products()); 
+
+            self.filteredProducts = ko.computed(() => {
+                const filteredProducts = ko.utils.arrayFilter(this.products(), product => !product.IsDeleted());
+                debugger;
+                return filteredProducts;
+            });            
         }
     }
 
@@ -27,6 +35,23 @@ class ProductsViewModel {
 
     removeProduct = (product) => {
         const self = this;
-        self.products.remove(product);
+        debugger;
+        // Remove the product if id is negative
+        if (product.Id() < 0) {
+            self.products.remove(product);
+        }
+
+        // update number column & IsDeleted to true if Id is not negative.
+        // We don't want to remove the to-be-deleted original record. 
+        // Because we want to be able to tell backend the to-be-deleted original record(s)
+        let number = 1;
+        ko.utils.arrayForEach(self.filteredProducts(), _product => {
+            if ( (product.Id() === _product.Id()) && (_product.Id() > 0) ){
+                _product.IsDeleted(true);
+            } else {
+                _product.Number(number);
+                number++;
+            }
+        });
     }
 }
