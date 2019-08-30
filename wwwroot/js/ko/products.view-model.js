67d4d755bb8a66c5069027038e19723c9bb21687
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-08-30 22:56:14 +0800

    Implement addNewProduct method

--- wwwroot/js/ko/products.view-model.js
+++ wwwroot/js/ko/products.view-model.js
@@ -7,8 +7,7 @@ class ProductsViewModel {
                 const numberedProduct = {                    
                     Number: ko.observable(_number),
                     ...p,
-                    // IsDirty: ko.observable(false),
-                    IsDeleted: ko.observable(false),
+                    IsDirty: ko.observable(false),
                 };
 
                 _number++;
@@ -24,11 +23,32 @@ class ProductsViewModel {
     }
 
     addNewProduct() {
-        alert('Add new product is not implemented !');
+        const self = this;
+        // TODO: Check if the last row is a new entry and all fields have been filled by user
+        const _products = self.products();
+        const lastRow = _products[_products.length - 1];
+        if (lastRow.Id() >= 0) {
+            const newProduct = {
+                Number: self.filteredProducts().length+1,
+                Id: -1,
+                Name: ``,
+                Price: 0,
+                Amount: 1,
+                IsDirty: false,
+                _destroy: false
+            };
+            self.products.push(newProduct);
+        }
     }
 
     doSubmit() {
         alert('Submit Products to server is not implemented !');
+        // const self = this;
+        // $.ajax("/Products", {
+        //     data: ko.toJSON({ Products: self.products }),
+        //     type: "post", contentType: "application/json",
+        //     success: result => alert(result)
+        // });        
     }
 
     removeProduct = (product) => {
